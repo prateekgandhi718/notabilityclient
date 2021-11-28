6f77b3cd113f831542f8f01e6d1fc1e22753320a
Prateek <prateekgandhi718@gmail.com>
2021-11-28 14:48:25 +0530

    changes in about section

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
             <hr />
             <div className="container">
                 <h1>What is Notability on Cloud?</h1>
-                <p>Well, it is basically a note taking app which stores your notes safely on the cloud. It has the ability to store your precious note title along with its description as well as a tag.</p>
+                <p>Well, it is basically a note taking app which stores your notes safely on the cloud. It has the ability to store your precious note title along with its description as well as a tag. More features are imminent and inevitable.</p>
             </div>
             <div className="container">
                 <h1>What are the technologies used to build this app?</h1>
@@ -21,7 +21,7 @@ const About = () => {
                 <h1>About the dev.</h1>
                 <div className="container d-flex">
                     <img style={{ height: "150px", borderRadius: "100px" }} src={require("./perfil.png").default} alt="" />
-                    <p>Prateek has multiple projects in Web Development under his belt. Although he rarely wears a real belt, his virtual belt of projects is always growing. He will probably always be a developer at heart. A growing interest in React Native has led him to help his sister-in-law in making an application for her business. </p>
+                    <p>Prateek has multiple projects in Web Development under his belt. Although he rarely wears a real belt, his virtual belt of projects is always growing. He will probably always be a developer at heart. </p>
                 </div>
                 <hr />
                 <p>See his website: <strong> www.prateekgandhi.in</strong> </p>
