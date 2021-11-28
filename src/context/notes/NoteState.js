3735ce2a489a475daa9fea5424275dd5353e1be3
Prateek <prateekgandhi718@gmail.com>
2021-11-28 11:24:55 +0530

    added the server API link in frontend

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import noteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000"
+    const host = "https://notabilityoncloud.herokuapp.com/"
     const notesInitial = []
     const [notes, setnotes] = useState(notesInitial)
 
