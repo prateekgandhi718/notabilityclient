8c8049473257c4c63d671db745d670f9099d58bf
Prateek <prateekgandhi718@gmail.com>
2021-11-29 10:51:51 +0530

    added greeting function in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,49 @@
-
+import React from 'react'
 import Notes from './Notes';
+import { useContext } from "react";
+import noteContext from '../context/notes/noteContext';
+import { useHistory } from "react-router";
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Home = (props) => {
+    const context = useContext(noteContext);
+    const { user, getUser } = context;
+    const history = useHistory();
+
+    const [time, settime] = useState("");
+    const myDate = new Date();
+
+    const getTheTime = (myDate) =>{
+        if (myDate.getHours() < 12) {
+            settime('morning');
+        }
+        else if (myDate.getHours() > 12 & myDate.getHours() < 17){
+            settime('afternoon');
+        }
+        else{
+            settime('evening');
+        }
+    }
     
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            getUser();
+            getTheTime(myDate);
+            
+        }
+        else{
+            history.push('/login')
+        }
+        // eslint-disable-next-line
+    }, [])
+
+
 
     return (
         <div>
-
+            <h1 className ="mx-2 my-1">Good {time}, {user.name}.</h1>
+            <h5 className = "mx-3 my-2">It's {myDate.toLocaleString('default', {weekday: 'long'})}, {myDate.toLocaleString('default', {month: 'long'})} {myDate.getDate()} </h5>
             <Notes showAlertProp = {props.showAlertProp} />
 
         </div>
